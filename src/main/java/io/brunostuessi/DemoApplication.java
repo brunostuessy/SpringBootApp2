84acf702e7867f3f6afb353466ef0dbda36ca546
brunostuessy <bruno.stuessi@gmail.com>
2024-11-09 21:41:17 +0100

    Rename packages

--- src/main/java/io/brunostuessi/DemoApplication.java
+++ src/main/java/io/brunostuessi/DemoApplication.java
@@ -1,10 +1,9 @@
-package com.example.demo;
+package io.brunostuessi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @SpringBootApplication
