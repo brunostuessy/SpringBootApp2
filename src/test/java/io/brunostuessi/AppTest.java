6622137037ac6ef29c63f5362965069ad849ea11
brunostuessy <bruno.stuessi@gmail.com>
2024-11-13 16:44:25 +0100

    Refactor

--- src/test/java/io/brunostuessi/AppTest.java
+++ src/test/java/io/brunostuessi/AppTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AppTests {
+class AppTest {
 
     @Autowired
     private MockMvc mockMvc;
